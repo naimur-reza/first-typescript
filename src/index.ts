9ea186827bbfcee7e78d761ea5aca37b22085832
Naimur Reza <naimur.rezaa@gmail.com>
2023-08-03 23:42:50 +0600

    add new

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,16 @@ let number: number[] = [];
 number[0] = 1;
 // number[1] = '2'; => this is can not assignable;
 
+// defining method
+let users: [number, string] = [20, "Anik"]; 
+
+
+/**
+ * Built in types in Typescript:
+ * 1. Any
+ * 2. Unknown
+ * 3. Enum
+ * 4. Never
+ * 5. Tuple
+ */
 
