0f4d5a46c7ae2e70e7b84c9cd4ee5037957e66b0
Naimur Reza <naimur.rezaa@gmail.com>
2023-11-03 21:03:37 +0600

    Add examples of extending props using type and interface in TypeVsInterface module

--- src/module2/TypeVsInterface.ts
+++ src/module2/TypeVsInterface.ts
@@ -9,9 +9,31 @@
 
   type Person2 = string; // type is used to all primitive types and non primitive types
 
+  type NewProps = {
+    id: number;
+  };
+
+  type SpecialPerson = Person1 & NewProps; // in this way we can extend props using type
+
+  const person: SpecialPerson = {
+    id: 230300203,
+    name: "Anik",
+    age: 20,
+  };
+
   // Interface
   interface Person3 {
     name: string;
     age: number;
   } // interface is used to all non primitive types
+
+  interface NewProps2 extends Person3 {
+    id: number;
+  } // in this way we can extend props using interface
+
+  const person2: NewProps2 = {
+    id: 23232,
+    name: "Reza",
+    age: 20,
+  };
 }
