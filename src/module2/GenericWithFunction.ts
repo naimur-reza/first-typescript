fedbb627a67a54510f09a440abadfce0fd55a0fc
Naimur Reza <naimur.rezaa@gmail.com>
2023-11-04 18:26:40 +0600

    re useable generic function

--- src/module2/GenericWithFunction.ts
+++ src/module2/GenericWithFunction.ts
@@ -17,4 +17,13 @@
     email: "naimur@gmail.com";
     age: 20;
   }>;
+
+  // generic with tuple
+
+  const createStudentWithTuple = <T, U>(param: T, param2: U): [T, U] => {
+    return [param, param2];
+  };
+
+  const newPerson = createStudentWithTuple("Anik", 20);
+  const newPerson2 = createStudentWithTuple<string, number>("Banti", 20);
 }
