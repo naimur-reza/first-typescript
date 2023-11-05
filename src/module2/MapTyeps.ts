93b8f520dd3c9b783d8fcb0dd4806ca67bd7d3d2
Naimur Reza <naimur.rezaa@gmail.com>
2023-11-05 11:41:55 +0600

    Add generic map type

--- src/module2/MapTyeps.ts
+++ src/module2/MapTyeps.ts
@@ -3,8 +3,22 @@ interface AreaString {
   width: string;
 }
 
+// this is lookup type
+
+type Height = AreaString["height"];
+
 // make this area string to number using type map
 
 type AreaNumber = {
   [key in keyof AreaString]: number;
+};
+
+// this is generic re useable map type
+type AreaCustom<T> = {
+  [key in keyof T]: T[key];
+};
+
+const area: AreaCustom<{ height: number; width: string }> = {
+  width: "100",
+  height: 200,
 };
